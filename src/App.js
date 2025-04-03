95caeae55058f41de1c0fa2775b43c16091b3786
MacCode7110 <mattmcal88@gmail.com>
2025-04-03 17:10:48 -0400

    Add items prop to new Accordion component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import Button from './components/Button';
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Accordion from './components/Accordion';
 
 function App() {
-    return <div>
-        <div>
-            <Button success outline rounded className="mb-5">
-                <GoBell className="mr-1"/>
-                    Click me!
-            </Button>
-        </div>
-        <div>
-            <Button danger outline>
-                <GoCloudDownload className="mr-1"/>
-                    Buy now!
-            </Button>
-        </div>
-        <div>
-            <Button warning>
-                <GoDatabase />
-                    See deal!
-            </Button>
-        </div>
-        <div>
-            <Button secondary outline>Hide ads!</Button>
-        </div>
-        <div>
-            <Button primary rounded>
-                Something!
-            </Button>
-        </div>
-    </div>;
+    const items = [
+        {
+            label: 'React',
+            content: 'Use it!'
+        },
+        {
+            label: 'JavaScript',
+            content: 'Use it!'
+        },
+        {
+            label: 'CSS',
+            content: 'Use it!'
+        }
+    ]
+    return <Accordion items={items}/>;
 }
 
 export default App;
