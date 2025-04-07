42e630e003dbf23ce97b27147a10efb82b41ca4a
MacCode7110 <mattmcal88@gmail.com>
2025-04-07 16:13:12 -0400

    Add isExpanded piece of state to Accordion component and render item content that is expanded

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+
 function Accordion({ items }) {
-    const renderedItems = items.map((item) => {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        // Set isExpanded to true if the index of the current item is equal to the expandedIndex, and set to false otherwise:
+        const isExpanded = index === expandedIndex;
+
+        // If isExpanded is false then content is false. If isExpanded is true and item.content is undefined then content is also undefined. If isExpanded is true and item.content is defined then content stores the div element containing the content of the item:
+        // const content = isExpanded && <div>{item.content}</div>;
+
+        // Conditional rendering using the isExpanded field
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
