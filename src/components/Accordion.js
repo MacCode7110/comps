ef2e7b104d02dae7a657f9971c43b544920f86f9
MacCode7110 <mattmcal88@gmail.com>
2025-04-18 17:01:35 -0400

    Add icons and more styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
     // Place the handleClick event handler outside of the mapping function everytime an external event handler function is needed.
     // This handleClick event handler is called manually because it is receiving an index as the first argument instead of an event object as the first argument.
     // Typically, the handleClick event handler receives an event object as the first argument. In this case, handleClick is not called manually.
     const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex);
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        } else {
+            setExpandedIndex(nextIndex);
+        }
     };
 
     const renderedItems = items.map((item, index) => {
@@ -17,16 +22,22 @@ function Accordion({ items }) {
         // If isExpanded is false then content is false. If isExpanded is true and item.content is undefined then content is also undefined. If isExpanded is true and item.content is defined then content stores the div element containing the content of the item:
         // const content = isExpanded && <div>{item.content}</div>;
 
+        const icon = <span className="text-2xl">
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </span>;
+
         // Conditional rendering using the isExpanded field
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)}>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+                    {item.label}
+                    {icon}</div>
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
     });
 
-    return <div>{renderedItems}</div>;
+    return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
