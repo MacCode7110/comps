8095f006ec0166a5c2d74816ebd10714e9ad522e
MacCode7110 <mattmcal88@gmail.com>
2025-05-09 14:43:54 -0400

    Add Dropdown component and options array to App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 
 function App() {
-    const items = [
-        {
-            id: 'l2kj5',
-            label: 'React',
-            content: 'Use it!'
-        },
-        {
-            id: 'lk2j35lkj',
-            label: 'JavaScript',
-            content: 'Use it!'
-        },
-        {
-            id: 'l1kj2i0g',
-            label: 'CSS',
-            content: 'Use it!'
-        }
-    ]
-    return <Accordion items={items}/>;
+    const options = [
+        {label: 'Red', value: 'red'},
+        {label: 'Green', value: 'green'},
+        {label: 'Blue', value: 'blue'}
+    ];
+
+    return <Dropdown options={options} />;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,24 @@
+import Accordion from './components/Accordion';
+
+function AccordionPage() {
+    const items = [
+        {
+            id: 'l2kj5',
+            label: 'React',
+            content: 'Use it!'
+        },
+        {
+            id: 'lk2j35lkj',
+            label: 'JavaScript',
+            content: 'Use it!'
+        },
+        {
+            id: 'l1kj2i0g',
+            label: 'CSS',
+            content: 'Use it!'
+        }
+    ]
+    return <Accordion items={items}/>;
+}
+
+export default AccordionPage;
