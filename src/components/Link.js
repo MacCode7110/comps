7650cf120edf8ebb4e22359b6bfe424664626776
MacCode7110 <mattmcal88@gmail.com>
2025-06-17 17:31:27 -0400

    Add use-navigation hook to Link and Route components

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,15 +1,23 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import classNames from 'classnames';
+import useNavigation from '../hooks/use-navigation';
 
 function Link({ to, children }) {
-    const { navigate } = useContext(NavigationContext);
+    const { navigate } = useNavigation();
+
+    const classes = classNames('text-blue-500');
 
     const handleClick = (event) => {
+        // If the user is holding down the metaKey or ctrlKey, then allow the browser to go through the normal navigation behavior and attempt to open up a new tab:
+        if (event.metaKey || event.ctrlKey) {
+            // Break statement
+            return;
+        }
         // Prevent normal broswer navigation behavior
         event.preventDefault();
+
         navigate(to);
     }
-    return <a onClick={handleClick}>{children}</a>
+    return <a className={classes} href={to} onClick={handleClick}>{children}</a>
 }
 
 export default Link;
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,4 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 function Route({ path, children }) {
     const { currentPath } = useContext(NavigationContext);
